83152188c8b14e78b965f1e19c4d1fd72d6b817b
Jakomu <janne.komul@gmail.com>
2022-08-13 08:50:49 +0300

    3.2 ready

--- index.js
+++ index.js
@@ -28,6 +28,14 @@ app.get("/api/persons", (req, res) => {
   res.json(persons);
 });
 
+app.get("/info", (req, res) => {
+  const personAmount = persons.length;
+  const time = Date();
+  res.send(
+    `Phonebook has info for ${personAmount} people` + "<br/><br/>" + `${time}`
+  );
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
