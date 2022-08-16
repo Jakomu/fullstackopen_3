d8cf3ad27e478eabe3a76d5f4986d80b82135a33
Jakomu <janne.komul@gmail.com>
2022-08-16 23:10:43 +0300

    3.9 ready

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 const morgan = require("morgan");
+const cors = require("cors");
+app.use(cors());
 
 morgan.token("body", (req, res) => {
   return JSON.stringify(req.body);
